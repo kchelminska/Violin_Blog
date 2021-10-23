f39ca1071d01c1e79dba637b0357cb391a186507
Katarzyna Chełmińska <chelminska.katarzyna@protonmail.com>
2021-10-23 23:09:12 +0200

    styled header section

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 
 const showNav = () => {
+    menu.classList.toggle('active');
     burger.classList.toggle('active');
 }
 burger.addEventListener('click', showNav);
